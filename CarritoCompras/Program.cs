490d46dffe16402d87d00e552ba2e596ff882fb9
Lu-Luou <rubertolh07@gmail.com>
2025-06-14 16:34:19 -0300

    init menu interactivo en program.cs

--- CarritoCompras/Program.cs
+++ CarritoCompras/Program.cs
@@ -14,6 +14,40 @@ namespace CarritoCompras
 
             Tienda panaderia = new Tienda();
             panaderia.CargarDatos(args[2] == "3" ? 3 : 0);
+
+            menu_interactivo(verduleria, carniceria, panaderia);
+        }
+
+        public static void menu_interactivo(Tienda tienda1, Tienda tienda2, Tienda tienda3)
+        {
+            while (true)
+            {
+                Console.WriteLine("Seleccione una tienda:");
+                Console.WriteLine("1. Verdulería");
+                Console.WriteLine("2. Carnicería");
+                Console.WriteLine("3. Panadería");
+                Console.WriteLine("4. Salir");
+
+                string opcion = Console.ReadLine() ?? string.Empty;
+
+                switch (opcion)
+                {
+                    case "1":
+                        tienda1.menu_interactivo_tienda();
+                        break;
+                    case "2":
+                        tienda2.menu_interactivo_tienda();
+                        break;
+                    case "3":
+                        tienda3.menu_interactivo_tienda();
+                        break;
+                    case "4":
+                        return;
+                    default:
+                        Console.WriteLine("Opción no válida, intente nuevamente.");
+                        break;
+                }
+            }
         }
     }
 }
