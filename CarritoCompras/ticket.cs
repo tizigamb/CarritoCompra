c96ddba3fd4bb31cf45c92c46835c2dc5fd081ef
Lu-Luou <rubertolh07@gmail.com>
2025-06-14 16:20:09 -0300

    Limpieza de ticket

--- CarritoCompras/ticket.cs
+++ CarritoCompras/ticket.cs
@@ -27,50 +27,5 @@ namespace CarritoCompras
             this.total = carrito.total_a_pagar();
             this.items_comprados = carrito;
         }
-
-        /*public void agregar_producto(ItemCarrito producto)
-        {
-            if (producto == null)
-            {
-                throw new ArgumentNullException(nameof(producto), "El producto no puede ser nulo.");
-            }
-
-            // Verificar si el producto ya existe en el ticket
-            var itemExistente = productos.FirstOrDefault(p => p.producto.id == producto.producto.id);
-            if (itemExistente != null)
-            {
-                itemExistente.cantidad += producto.cantidad;
-            }
-            else
-            {
-                productos.Add(producto);
-            }
-
-            // Recalcular total
-            decimal subtotal = productos.Sum(item =>
-            {
-                decimal itemSubtotal = item.producto.precio * item.cantidad;
-                if (item.cantidad >= 5)
-                {
-                    itemSubtotal *= 0.85m;
-                }
-                return itemSubtotal;
-            });
-            this.total = subtotal * 1.21m;
-        }
-
-        public void total_a_pagar()
-        {
-            Console.WriteLine("\n");
-            Console.WriteLine("\n" + total);
-        }
-
-        public void contenido_carrito()
-        {
-            foreach (var item in productos)
-            {
-                Console.WriteLine($"Nombre: {item.producto.nombre}, Cantidad: {item.cantidad}, Precio unitario: {item.producto.precio}, Subtotal: {item.cantidad * item.producto.precio}");
-            }
-        }*/
     }
 }
